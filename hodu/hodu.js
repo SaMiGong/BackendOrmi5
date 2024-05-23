213a89a405536f34381556a8dfa7348af19ff5aa
MoonSeokJun <tjrwns2513@naver.com>
2024-05-23 17:10:22 +0900

    img

--- hodu/hodu.js
+++ hodu/hodu.js
@@ -4,18 +4,18 @@ rollUp = () => {
     window.scrollTo({ top: Top, behavior: 'smooth'})
 }
 
-const imgMore = document.getElementById('.more_img');
-const showMore = document.getElementById('.show_more');
+const imgMore = document.getElementById('more_img');
+const showMore = document.getElementById('show_more');
 const imgAddNum = 6;
 
 showMore.addEventListener('click', () => {
     if (imgMore.style.display === 'none') {
         imgMore.style.display = 'block';
-        showMore.textContent = 'Show less';
+        showMore.textContent = 'Show Less';
 
         // Picsum Photos 에서 이미지 가져오기
         for (let i = 0; i < imgAddNum; i++) {
-            const response = await fatch(`https://picsum.photos/v2/list?page=2&limit=${i}`);
+            const response = await fatch(`https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/500/300`);
             const blob = await response.blob();
             const imageUrl = URL.createObjectURL(blob);
             const img = document.createElement('img');
@@ -25,5 +25,8 @@ showMore.addEventListener('click', () => {
     } else {
         imgMore.style.display = 'none';
         showMore.textContent = 'Show More';
+
+        // 이미지들 제거
+        imgMore.innerHTML = '';
     }
 });
